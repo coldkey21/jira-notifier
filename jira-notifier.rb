a561a7fe397840316fc092be04fa90c6a1a81490
mbirky <coldkey.21@gmail.com>
2016-03-08 14:13:09 -0800

    Moving the message displayed to the json file

--- jira-notifier.rb
+++ jira-notifier.rb
@@ -37,7 +37,7 @@ class Notifier
             issues = @jira_client.Issue.jql(notification["jira_query"])
 
             if issues.any?
-                output = "<" + options[:jira_server] + "/issues/?jql=" + URI.escape(notification["jira_query"]) + "|The following issues need fix versions:>\n"
+                output = "<" + options[:jira_server] + "/issues/?jql=" + URI.escape(notification["jira_query"]) + "|" + notification["message"].to_s + ">\n"
 
                 issues.each do |issue|
                     output += "\t* <" + options[:jira_server] + "/browse/" + issue.key + "|" + issue.key + "> - " + issue.summary.chomp(".") + "\n"
